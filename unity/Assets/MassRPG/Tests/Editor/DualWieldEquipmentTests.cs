8e629c7a0d4a9f3dce15f6198fb219ed751caaa3
TheMasterStick <87497026+TheMasterStick@users.noreply.github.com>
2026-09-16 01:43:16 +0300

    Fix dual-wield tests to exercise equipment requirements correctly

--- unity/Assets/MassRPG/Tests/Editor/DualWieldEquipmentTests.cs
+++ unity/Assets/MassRPG/Tests/Editor/DualWieldEquipmentTests.cs
@@ -1,6 +1,7 @@
 using System;
 using MassRPG.Core.Content;
 using MassRPG.Core.Inventory;
+using MassRPG.Core.Skills;
 using MassRPG.Data.Items;
 using NUnit.Framework;
 
@@ -14,13 +15,14 @@ namespace MassRPG.Tests
             var items = MigrationSeedItemCatalog.Create();
             var inventory = new InventoryState(8);
             var equipment = new EquipmentState();
+            var skills = new SkillSet();
             var sword = new ContentId("iron_sword");
             var dagger = new ContentId("iron_dagger");
             InventoryRules.AddItem(inventory, items, sword, 1);
             InventoryRules.AddItem(inventory, items, dagger, 1);
 
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 0, EquipmentSlot.MainHand).Success);
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 1, EquipmentSlot.OffHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 0, EquipmentSlot.MainHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 1, EquipmentSlot.OffHand).Success);
             Assert.AreEqual(sword, equipment.GetOrNull(EquipmentSlot.MainHand));
             Assert.AreEqual(dagger, equipment.GetOrNull(EquipmentSlot.OffHand));
         }
@@ -31,13 +33,14 @@ namespace MassRPG.Tests
             var items = MigrationSeedItemCatalog.Create();
             var inventory = new InventoryState(8);
             var equipment = new EquipmentState();
+            var skills = new SkillSet();
             InventoryRules.AddItem(inventory, items, new ContentId("iron_sword"), 1);
             InventoryRules.AddItem(inventory, items, new ContentId("iron_dagger"), 1);
             InventoryRules.AddItem(inventory, items, new ContentId("iron_2h_sword"), 1);
 
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 0, EquipmentSlot.MainHand).Success);
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 1, EquipmentSlot.OffHand).Success);
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 2, EquipmentSlot.MainHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 0, EquipmentSlot.MainHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 1, EquipmentSlot.OffHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 2, EquipmentSlot.MainHand).Success);
 
             Assert.AreEqual(new ContentId("iron_2h_sword"), equipment.GetOrNull(EquipmentSlot.MainHand));
             Assert.IsFalse(equipment.IsOccupied(EquipmentSlot.OffHand));
@@ -50,11 +53,12 @@ namespace MassRPG.Tests
             var items = MigrationSeedItemCatalog.Create();
             var inventory = new InventoryState(8);
             var equipment = new EquipmentState();
+            var skills = new SkillSet();
             InventoryRules.AddItem(inventory, items, new ContentId("iron_dagger"), 1);
             InventoryRules.AddItem(inventory, items, new ContentId("iron_shield"), 1);
 
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 0, EquipmentSlot.OffHand).Success);
-            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, 1, EquipmentSlot.OffHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 0, EquipmentSlot.OffHand).Success);
+            Assert.IsTrue(InventoryRules.EquipFromInventory(inventory, equipment, items, skills, 1, EquipmentSlot.OffHand).Success);
 
             Assert.AreEqual(new ContentId("iron_shield"), equipment.GetOrNull(EquipmentSlot.OffHand));
             Assert.AreEqual(1, inventory.CountItem(new ContentId("iron_dagger")));
